995fe820b9d7db88fed10ffbf3a3c076cc4d79dd
renaco <renanromero@gmail.com>
2018-05-28 23:37:09 -0500

    feat code: define fbonacci

---
+++ src/algorithms/math/fibonacci/fibonacci.js
@@ -0,0 +1,19 @@
+export default function fibonacci(numberPosition) {
+    if (numberPosition === 1) {
+        return 1;
+    }
+
+    let iterationsCounter = numberPosition - 1;
+    let fib = null;
+    let fibPrev = 1;
+    let fibPrevPrev = 0;
+
+    while (iterationsCounter) {
+        fib = fibPrev + fibPrevPrev;
+        fibPrevPrev = fibPrev;
+        fibPrev = fib;
+        iterationsCounter -= 1;
+    }
+
+    return fib;
+}
